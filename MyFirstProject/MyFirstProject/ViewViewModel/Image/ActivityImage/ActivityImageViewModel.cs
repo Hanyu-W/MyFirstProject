831652f91aafb2acf1ecd9745c94b32af066ea6b
HyWei787 <HyWei787@gmail.com>
2022-10-25 10:14:57 -0500

    10/25

--- MyFirstProject/MyFirstProject/ViewViewModel/Image/ActivityImage/ActivityImageViewModel.cs
+++ MyFirstProject/MyFirstProject/ViewViewModel/Image/ActivityImage/ActivityImageViewModel.cs
@@ -3,14 +3,23 @@ using MyFirstProject.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MyFirstProject.ViewViewModel.Image.ActivityImage
 {
     class ActivityImageViewModel : BaseViewModel
     {
+        public ImageSource ActivityImageSrc { get; set; }
         public ActivityImageViewModel()
         {
             Title = Titles.ActivityImageTitle;
+            this.GetActitivtyImageSrc();
+        }
+
+        private void GetActitivtyImageSrc()
+        {
+            ActivityImageSrc = ImageSource.FromResource("MyFirstProject.Image.azusa-nakano.jpg");
+
         }
     }
 }
