40096c3b1cc6570503e0127bc3c8976d2dacd636
HyWei787 <HyWei787@gmail.com>
2022-10-27 10:19:52 -0500

    10/27

---
+++ MyFirstProject/MyFirstProject/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        //default constructor
+        public Person()
+        {
+
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        //static method creates a list of names
+        public static List<Person> getNames()
+        {
+            return new List<Person>
+            {
+                new Person("Divyesh Kotthakota"),
+                new Person("Kevin Yu"),
+                new Person("Justin Lopato"),
+                new Person("Illia Myronov"),
+                new Person("Hanyu Wei")
+            };
+        }
+
+    }
+}
