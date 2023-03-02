d4d5af3c993b5b547cc1c478ac9b316949b21b70
Hanyu W <HyWei787@gmail.com>
2023-03-02 10:26:27 -0600

    3/2

---
+++ MyFirstProject/MyFirstProject/ViewViewModel/SQLite/Vacations/SQLiteAddVacationView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MyFirstProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstProject.ViewViewModel.SQLite.Vacations
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SQLiteAddVacationView : ContentPage
+    {
+        public SQLiteAddVacationView(Vacation vacation)
+        {
+            InitializeComponent();
+            this.BindingContext = new SQLiteAddVacationViewModel(vacation);
+        }
+    }
+}
--- MyFirstProject/MyFirstProject/ViewViewModel/SQLite/Vacations/SQLiteViewModel.cs
+++ MyFirstProject/MyFirstProject/ViewViewModel/SQLite/Vacations/SQLiteViewModel.cs
@@ -1,10 +1,74 @@
-﻿using System;
+﻿using MyFirstProject.Models;
+using MyFirstProject.Patterns.Repositories;
+using MyFirstProject.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MyFirstProject.ViewViewModel.SQLite.Vacations
 {
-    class SQLiteViewModel
+    public class SQLiteViewModel : BaseViewModel
     {
+        private ObservableCollection<Vacation> _vacationCollection;
+
+        public ObservableCollection<Vacation> VacationCollection
+        {
+            get { return _vacationCollection;  }
+            set
+            {
+                _vacationCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SQLiteViewModel()
+        {
+            VacationCollection = new ObservableCollection<Vacation>();
+            Task.Run(async () => await RefreshVacationListData());
+
+            MessagingCenter.Subscribe<object>(this, "RefreshData", async (sender) =>
+            {
+                await RefreshVacationListData();
+            });
+        }
+
+        private async Task RefreshVacationListData()
+        {
+            var vacation = DependencyService.Get<ISQLite>().GetVacation();
+            VacationCollection = new ObservableCollection<Vacation>(vacation);
+        }
+
+        public Command AddCommand
+        {
+            get
+            {
+                return new Command<Vacation>((Vacation vacation) =>
+                {
+                    Application.Current.MainPage.Navigation.PushAsync(new SQLiteAddVacationView(vacation));
+
+                    MessagingCenter.Subscribe<Vacation>(this, "AddVacation", async (data) =>
+                    {
+                        await RefreshVacationListData();
+
+                        MessagingCenter.Unsubscribe<Vacation>(this, "AddVacation");
+                    });
+                });
+            }
+        }
+
+        public Command<Vacation> DeleteCommand
+        {
+            get
+            {
+                return new Command<Vacation>((Vacation vacation) =>
+                {
+                    DependencyService.Get<ISQLite>().DeleteVacation(vacation.ID);
+                    VacationCollection.Remove(vacation);
+                });
+            }
+        }
     }
 }
